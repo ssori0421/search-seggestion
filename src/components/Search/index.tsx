eb51dbf9effb87e6d87a425d7ab924f15b084acc
ssori <115159126+ssori0421@users.noreply.github.com>
2023-06-30 22:59:21 +0900

    feat: SearchBar, SuggestionList 컴포넌트 분리

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,29 +1,14 @@
 import { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import getDisease from '../../services/getDisease.ts';
-import { IDisease } from '../../types/disease.ts';
-import useDebounce from '../../hooks/useDebounce.ts';
 import styles from './search.module.scss';
+import SearchBar from './SearchBar/index.tsx';
+import SearchSuggestion from './SuggestionList/index.tsx';
 
 function Search() {
   const [searchInput, setSearchInput] = useState<string>('');
-  const debounceInput = useDebounce(searchInput);
-
-  const { data: suggestDiseaseList } = useQuery<IDisease[], Error>(
-    ['diseases', debounceInput],
-    () => getDisease(debounceInput) as Promise<IDisease[]>,
-    {
-      enabled: debounceInput !== '', // 쿼리 활성화 여부 설정
-      staleTime: 60000, // 1분이 지나면 stale 해짐
-      cacheTime: 300000, // 캐시 시간 (5분)
-    }
-  );
-
   const onChangeInputDisease = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setSearchInput(value);
   };
-
   return (
     <div className={styles.searchContainer}>
       <div className={styles.searchWrapper}>
@@ -31,28 +16,8 @@ function Search() {
           국내 모든 임상시험 검색하고 <br />
           온라인으로 참여하기
         </h2>
-        <div className={styles.searchInputContainer}>
-          <div className={styles.searchInutWrapper}>
-            <input
-              className={styles.searchInput}
-              onChange={onChangeInputDisease}
-              placeholder="질환명을 입력해 주세요."
-            />
-            <div className={styles.sarchButton}>검색</div>
-          </div>
-        </div>
-        <div className={styles.suggestDiseaseListContainer}>
-          {suggestDiseaseList && (
-            <>
-              <span>추천 검색어</span>
-              <ul className={styles.suggestDiseaseList}>
-                {suggestDiseaseList?.map((disease: IDisease) => (
-                  <li key={disease.id}>{disease.name}</li>
-                ))}
-              </ul>
-            </>
-          )}
-        </div>
+        <SearchBar onChange={onChangeInputDisease} />
+        <SearchSuggestion searchInput={searchInput} />
       </div>
     </div>
   );
