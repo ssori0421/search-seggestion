0d03ae9d03dc4b9b18a2c19c7bd3db439a9bd00e
ssori <115159126+ssori0421@users.noreply.github.com>
2023-06-30 13:01:50 +0900

    feat: css module 적용

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 import getDisease from '../../services/getDisease.ts';
 import { IDisease } from '../../types/disease.ts';
 import useDebounce from '../../hooks/useDebounce.ts';
-import './search.scss';
+import styles from './search.module.scss';
 
 function Search() {
   const [searchInput, setSearchInput] = useState<string>('');
@@ -25,27 +25,27 @@ function Search() {
   };
 
   return (
-    <div className="searchContainer">
-      <div className="searchWrapper">
-        <h2 className="title">
+    <div className={styles.searchContainer}>
+      <div className={styles.searchWrapper}>
+        <h2 className={styles.title}>
           국내 모든 임상시험 검색하고 <br />
           온라인으로 참여하기
         </h2>
-        <div className="searchInputContainer">
-          <div className="searchInutWrapper">
+        <div className={styles.searchInputContainer}>
+          <div className={styles.searchInutWrapper}>
             <input
-              className="searchInput"
+              className={styles.searchInput}
               onChange={onChangeInputDisease}
               placeholder="질환명을 입력해 주세요."
             />
-            <div className="sarchButton">검색</div>
+            <div className={styles.sarchButton}>검색</div>
           </div>
         </div>
-        <div className="suggestDiseaseListContainer">
+        <div className={styles.suggestDiseaseListContainer}>
           {suggestDiseaseList && (
             <>
               <span>추천 검색어</span>
-              <ul className="suggestDiseaseList">
+              <ul className={styles.suggestDiseaseList}>
                 {suggestDiseaseList?.map((disease: IDisease) => (
                   <li key={disease.id}>{disease.name}</li>
                 ))}
